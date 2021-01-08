f212b7b45e1f0ffe824c50772cc43086d48c06ea
kroppel <konsti.roppel@t-online.de>
2021-01-08 11:44:28 +0100

    Fix last process id, add intermediate time measurement, set mem-per-cpu-option

--- scripts/distributed/warming_stripes_mpi_distributed.py
+++ scripts/distributed/warming_stripes_mpi_distributed.py
@@ -7,21 +7,7 @@ import time
 def get_data_states(path):
     data = list(map(lambda x: [x[i].strip() for i in [0] + np.arange(2,18).tolist()], map(lambda x: x.split(";"), open(path, "r").read().split("\n")[2:][:-1])))
     return data
-    
-def shuffle(data_dict, process_number, rank, key_ranges):
-    for foreign_rank in np.arange(0, process_number):
-        temp1 = {}
-        if foreign_rank != rank:
-            for key in key_ranges[foreign_rank]:
-                temp1[key] = data_dict[key]
-            request = comm.isend(temp1, dest=foreign_rank)
-            request.wait()
-            temp2 = {}
-            temp2.update(comm.irecv(source=i+1).wait())
-            for key in key_ranges[rank]:
-                data_dict[key] = data_dict[key] + temp2[key]
-
-
+     
 def reducer(data_dict):
     keys = list(data_dict.keys())
     for key in keys:
@@ -36,38 +22,39 @@ prog_start_time = time.time()
 
 # read in data completely
 
-data_path_hdfs = "data/data_oversized/"
-data_path_local = "/home/vi46six/warming_stripes_spark/data/data_oversized/"
+data_path_beegfs = "/beegfs/vi46six/data/data_oversized_v3/"
+data_path_local = "/home/vi46six/warming_stripes_spark/data/data_oversized_v3/"
 months = ["01","02","03","04","05","06","07","08","09","10","11","12"]
 data = []
         
 for month in months:
-    data.append(list(get_data_states(data_path_local + "regional_averages_tm_" + month + ".txt")))
+    data.append(list(get_data_states(data_path_beegfs + "regional_averages_tm_" + month + ".txt")))
 
 data_flattened = [pair for sublist in data for pair in sublist]
+
+# intermediate time measurement
+prog_intermediate_time = time.time()
 
 # data partitioning locally
 
 key_ranges = {}
 key_number = 2019-1881+1
 # number of total processes
-process_number = 4
+process_number = 60
 keys_per_process = math.ceil(key_number/process_number)
 # id of process that gets last partition assigned
-last_process = math.ceil(key_number/keys_per_process)
+last_process = math.ceil(key_number/keys_per_process) - 1
 
 
 for r in np.arange(0, process_number):
-    if r < (last_process-1):
+    if r < (last_process):
         key_ranges[r] = np.arange(1881+(r*keys_per_process), 1881+((r+1)*keys_per_process))
-    elif r == (last_process-1):
+    elif r == (last_process):
         key_ranges[r] = np.arange(1881+(r*keys_per_process), 2020)
     else:
         key_ranges[r] = np.asarray([])
 
-print(str(rank)+"; "+str(key_ranges))
-
-data_partitioned = [[int(pair[0]), float(pair[1])] for pair in data_flattened if int(pair[0]) in key_ranges[rank]] 
+data_partitioned = [[int(pair[0]), float(pair[1])] for pair in data_flattened if int(pair[0]) in key_ranges.get(rank, [])] 
 
 # processing
 
@@ -76,17 +63,15 @@ data_dict = {}
 for key in key_ranges[rank]:
     data_dict[key] = [pair[1] for pair in data_partitioned if pair[0]==key]
 
-#shuffle(data_dict, process_number, rank, key_ranges)
-
 data_dict = reducer(data_dict)
 
 # data aggregation on process 0
 
-if rank != 0:
+if rank != 0 and rank <= last_process:
     request = comm.isend(data_dict, dest=0)
     request.wait()
-else:
-    for i in np.arange(process_number-1):
+elif rank == 0:
+    for i in np.arange(last_process):
         data_dict.update(comm.irecv(source=i+1).wait())
     print(data_dict)
     
@@ -94,3 +79,5 @@ else:
     exec_time = prog_stop_time-prog_start_time
 
     print("Execution time:"+str(exec_time))
+    print("Data loading: "+str(prog_intermediate_time-prog_start_time))
+    print("Pure execution: "+str(prog_stop_time-prog_intermediate_time))
